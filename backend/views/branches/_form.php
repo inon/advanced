6b41f293ad1d6a8af8da8b2d201bb4b891db6a4c
Inon Baguio <inon.baguio@yahoo.com>
2017-11-05 08:45:21 +0800

    #14 used kartiv select2 date picker

--- backend/views/branches/_form.php
+++ backend/views/branches/_form.php
@@ -1,6 +1,7 @@
 <?php
 
 use backend\models\Companies;
+use kartik\select2\Select2;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
@@ -14,17 +15,20 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-
-    <?= $form->field($model, 'companies_company_id')->dropDownList(
-        ArrayHelper::map(Companies::find()->all(), 'company_id', 'company_name'),
-        ['prompt' => 'Select Company']
-    )  ?>
-
+    <?= $form->field($model, 'companies_company_id')->widget(Select2::classname(), [
+            'data' => ArrayHelper::map(Companies::find()->all(), 'company_id', 'company_name'),
+            'language' => 'en',
+            'options' => ['placeholder' => 'Select a company ...'],
+            'pluginOptions' => [
+                'allowClear' => true
+            ],
+        ]);
+    ?>
     <?= $form->field($model, 'branch_name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'branch_address')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'branch_status')->dropDownList([ 'active' => 'Active', 'inactive' => 'Inactive', ], ['prompt' => '']) ?>
+    <?= $form->field($model, 'branch_status')->dropDownList([ 'active' => 'Active', 'inactive' => 'Inactive', ], ['prompt' => 'Status']) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
